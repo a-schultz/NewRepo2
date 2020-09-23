7fcd6e977a5ea51013f1a9a2fdf1951c8d010c6f
kfgutf39 <71448046+kfgutf39@users.noreply.github.com>
2020-09-23 14:16:04 -0400

    Update Form1.cs
    I switch true to false

--- Group_Project/Form1.cs
+++ Group_Project/Form1.cs
@@ -19,7 +19,7 @@ namespace Group_Project
 
         private void button1_Click(object sender, EventArgs e)
         {
-            pictureBox1.Visible = true;
+            pictureBox1.Visible = false;
         }
     }
 }
